4a20196fb3e5557541a3ad76c3dd5c51a5b20be2
gloomya <gloomya@gmail.com>
2019-10-24 21:17:14 -0400

    features and team social icons

--- js/main.js
+++ js/main.js
@@ -1,31 +1,3 @@
 $(document).ready(function() {
-    // Show / Hide social profiles Team Section
-    // #1 Card
-    $('#card-1').hide();
-    $('#card-img-1').hover(function() {
-        $('#card-1').show();
-            }, function() {
-                $('#card-1').hide();
-            });
-    // #2 Card
-    $('#card-2').hide();
-    $('#card-img-2').hover(function() {
-        $('#card-2').show();
-            }, function() {
-                $('#card-2').hide();
-            });
-    // #3 Card   
-    $('#card-3').hide();    
-    $('#card-img-3').hover(function() {
-        $('#card-3').show();
-            }, function() {
-                $('#card-3').hide();
-            });
-    // #4 Card
-    $('#card-4').hide();
-    $('#card-img-4').hover(function() {
-        $('#card-4').show();
-            }, function() {
-                $('#card-4').hide();
-            });
+
 });
