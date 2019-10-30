68c39502b37a0d41a55e49b9e2c7e66f33bc3dcb
gloomya <gloomya@gmail.com>
2019-10-29 20:59:57 -0400

    animations and styles

--- js/main.js
+++ js/main.js
@@ -26,4 +26,53 @@ $(document).ready(function() {
         mobile: false
     });
     wow.init();
+// Dark mode switcher
+    $("#switcher").click(function() {
+        if ($('body').css('background-color')=='white') {
+            $('<link rel="stylesheet" href="/css/dark.css" type="text/css" />')
+            .appendTo('head');
+        } else {
+            $('link[href="/css/dark.css"]').remove();
+        } 
+    });
+    
+
+// Particles 
+var mousePos = {};
+
+ function getRandomInt(min, max) {
+   return Math.round(Math.random() * (max - min + 1)) + min;
+ }
+  
+  $('#hero').mousemove(function(e) {
+    mousePos.x = e.pageX;
+    mousePos.y = e.pageY;
+  });
+  
+  $('#hero').mouseleave(function(e) {
+    mousePos.x = -1;
+    mousePos.y = -1;
+  });
+  
+  var draw = setInterval(function(){
+    if(mousePos.x > 0 && mousePos.y > 0){
+      
+      var range = 15;
+      
+      var color = "background: rgb("+getRandomInt(0,255)+","+getRandomInt(0,255)+","+getRandomInt(0,255)+");";
+      
+      var sizeInt = getRandomInt(10, 30);
+      size = "height: " + sizeInt + "px; width: " + sizeInt + "px;";
+      
+      var left = "left: " + getRandomInt(mousePos.x-range-sizeInt, mousePos.x+range) + "px;";
+      
+      var top = "top: " + getRandomInt(mousePos.y-range-sizeInt, mousePos.y+range) + "px;"; 
+
+      var style = left+top+color+size;
+      $("<div class='ball' style='" + style + "'></div>").appendTo('#wrap').one("webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend", function(){$(this).remove();}); 
+    }
+  }, 60);
+
 });
+
+
